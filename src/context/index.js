46bfe571c0ade7f44c99499d1abb2f060ef779f3
Kevin Flanagan <kevin@kflan.io>
2020-02-20 16:04:07 -0700

    context attempted

--- src/context/index.js
+++ src/context/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-// import { AuthProvider } from "./auth-context.js";
-// import { UserProvider } from "./user-context.js";
+import { AuthProvider } from "./auth-context.js";
+import { UserProvider } from "./user-context.js";
 
 function AppProviders({ children }) {
   return (
     <>
-      {/* <AuthProvider> */}
-      {/* <UserProvider>{children}</UserProvider> */}
-      {children}
-      {/* </AuthProvider> */}
+      <AuthProvider>
+        <UserProvider>{children}</UserProvider>
+        {/* {children} */}
+      </AuthProvider>
     </>
   );
 }
